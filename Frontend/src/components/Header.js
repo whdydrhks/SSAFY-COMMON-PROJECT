0c1265b6cbbd48afd405c277482b791420108061
kwnoyng <kwnoyng@gmail.com>
2023-01-21 19:26:43 +0900

    [FE] style : Main initial styling

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -2,21 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const SHeaderForm = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-const SHeaderAppName = styled.div``;
+const SHeaderForm = styled.div``;
 const SHeaderSignForm = styled.div`
   display: flex;
+  justify-content: right;
+  position: relative;
+  margin-top: 16px;
+  margin-right: 40px;
 `;
-const SHeaderSignElement = styled.div``;
+const SHeaderSignElement = styled.div`
+  padding: 0 32px;
+  a {
+    text-decoration: none;
+    color: grey;
+  }
+`;
 function HomeHeader() {
   return (
     <SHeaderForm>
-      <SHeaderAppName>
-        <Link to="/home">반가워</Link>
-      </SHeaderAppName>
       <SHeaderSignForm>
         <SHeaderSignElement>
           <Link to="/signIn">로그인</Link>
--- Frontend/src/components/Nav.js
+++ Frontend/src/components/Nav.js
@@ -2,34 +2,56 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const SNavForm = styled.div`
+const SNav = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  position: relative;
+  margin-top: 24px;
 `;
-const SNavElement = styled.div``;
+const SNavApp = styled.div`
+  margin-left: 40px;
+  padding: 0 32px;
+  font-size: 40px;
+`;
+const SNavForm = styled.div`
+  display: flex;
+`;
+const SNavElement = styled.div`
+  a {
+    margin-right: 40px;
+    padding: 0 24px;
+    text-decoration: none;
+    color: black;
+    font-size: 24px;
+  }
+`;
 
 function Nav() {
   return (
-    <SNavForm>
-      <SNavElement>
-        <Link to="/about">소개</Link>
-      </SNavElement>
-      <SNavElement>
-        <Link to="/news">소식지</Link>
-      </SNavElement>
-      <SNavElement>
-        <Link to="/live">Live</Link>
-      </SNavElement>
-      <SNavElement>
-        <Link to="/adoption">입양</Link>
-      </SNavElement>
-      <SNavElement>
-        <Link to="/community">게시판</Link>
-      </SNavElement>
-      <SNavElement>
-        <Link to="/donation">후원</Link>
-      </SNavElement>
-    </SNavForm>
+    <SNav>
+      <SNavApp>반가워</SNavApp>
+      <SNavForm>
+        <SNavElement>
+          <Link to="/about">소개</Link>
+        </SNavElement>
+        <SNavElement>
+          <Link to="/news">소식지</Link>
+        </SNavElement>
+        <SNavElement>
+          <Link to="/live">Live</Link>
+        </SNavElement>
+        <SNavElement>
+          <Link to="/adoption">입양</Link>
+        </SNavElement>
+        <SNavElement>
+          <Link to="/community">게시판</Link>
+        </SNavElement>
+        <SNavElement>
+          <Link to="/donation">후원</Link>
+        </SNavElement>
+      </SNavForm>
+    </SNav>
   );
 }
 
