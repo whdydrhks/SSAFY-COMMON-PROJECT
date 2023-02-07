656ddf90ca1ce50a6e21bb800e0a4a3307fcf252
haninan94 <yuyu1172@gmail.com>
2023-02-07 15:01:07 +0900

    [FE] feat : Animal api test

--- FrontEnd/src/components/Animal/AnimalList.js
+++ FrontEnd/src/components/Animal/AnimalList.js
@@ -1,10 +1,18 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
+
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+
 // import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { animalListState } from '../../recoilState';
 import AnimalItem from './AnimalItem';
+import API_URL from '../../api/api';
 
 const SLink = styled(Link)`
   margin: 0 auto;
@@ -15,28 +23,27 @@ const SLink = styled(Link)`
 function AnimalList(props) {
   const animalList = useRecoilValue(animalListState);
   // console.log(animalList);
-  console.log(props);
+  // console.log(props);
 
-  // const [temp, setTemp] = useState([])
+  const [temp, setTemp] = useState([]);
 
-  // const getAnimalList = async () => {
-  //   const filteredAnimalList = await.axios.get(
-  //     url
-  //   );
-  //   setTemp(filteredAnimalList.data)
-
-  // }
-  // console.log(temp)
-  // useEffect(() => {
-  //   getAnimalList()
-  // }, [])
-  // axios.get(url, { params: { expired: props.expired } }).then(res => {
-  //   console.log(res);
-  // });
+  const getAnimalList = async () => {
+    const filteredAnimalList = await axios.get(
+      `${API_URL}/shelter/1/animal?pageNo=1`,
+    );
+    setTemp(filteredAnimalList.data);
+  };
+  console.log(temp);
+  useEffect(() => {
+    getAnimalList();
+  }, []);
+  axios.get(url, { params: { expired: props.expired } }).then(res => {
+    console.log(res);
+  });
 
   return (
     <div>
-      {animalList.map(animalItem => (
+      {temp.map(animalItem => (
         <SLink
           to={`/animal/${animalItem.animalId}`}
           key={animalItem.animalId}
