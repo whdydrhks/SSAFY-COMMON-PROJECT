36c784ea14d288e8c9853e5e9a9dc035a7c7615e
kwnoyng <kwnoyng@gmail.com>
2023-02-10 03:37:09 +0900

    [FE] style : Styling schedule

--- FrontEnd/src/components/common/Header.js
+++ FrontEnd/src/components/common/Header.js
@@ -4,29 +4,55 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import LogoutIcon from '@mui/icons-material/Logout';
 import styled from 'styled-components';
 import axios from 'axios';
-import { useRecoilState, useResetRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue, useResetRecoilState } from 'recoil';
 import { authStateAtom, userAtom } from '../../recoilState';
 import { getCookie, removeCookie } from '../../pages/Account/cookie';
+import helloIcon from '../../images/logo/helloIcon.png';
 import API_URL from '../../api/api';
 import '../../styles/cafe24.css';
 
 const SAppBar = styled(AppBar)`
   position: fixed;
   padding: 4px;
-  background-color: rgba(55, 155, 155, 0.2) !important;
+  background-color: rgba(180, 230, 230) !important;
   a {
     font-family: 'cafe24';
     text-decoration: none;
     color: black;
   }
 `;
+const SHomeLogo = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    display: flex;
+    align-items: center;
+  }
+`;
+const SImg = styled.img`
+  width: 2rem;
+  margin-right: 0.5rem;
+`;
+const SHello = styled.span`
+  font-size: 1.7rem;
+`;
+
+const SButton = styled.button`
+  background-color: rgba(180, 230, 230) !important;
+  font-family: 'cafe24';
+  border: none;
+  color: black;
+  margin-left: 12px;
+`;
 
 function Header() {
   const accessToken = getCookie('accessToken');
   const [authState, setAuthState] = useRecoilState(authStateAtom);
+  const user = useRecoilValue(userAtom);
   const resetUser = useResetRecoilState(userAtom);
   const navigate = useNavigate();
 
@@ -57,15 +83,22 @@ function Header() {
       <SAppBar>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link to="/">반가워</Link>
+            <SHomeLogo>
+              <Link to="/">
+                <SImg src={helloIcon} alt="hello" />
+              </Link>
+              <Link to="/">
+                <SHello>반가워</SHello>
+              </Link>
+            </SHomeLogo>
           </Typography>
-          <Button color="inherit">
-            <Link to="/alarm">Alarm</Link>
-          </Button>
+          <Link to={`/alarm/${user.userId}`}>
+            <NotificationsActiveIcon />
+          </Link>
           {!authState ? null : (
-            <Button color="inherit" onClick={handleLogout}>
-              로그아웃
-            </Button>
+            <SButton color="inherit" onClick={handleLogout}>
+              <LogoutIcon />
+            </SButton>
           )}
         </Toolbar>
       </SAppBar>
